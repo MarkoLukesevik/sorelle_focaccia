e2d238b2cf30217d3018b151ec5c39885ff0065c
Marko <marko.lukesevik@protonmail.com>
2024-11-17 14:08:01 +0100

    WIP

--- src/components/products/Products.vue
+++ src/components/products/Products.vue
@@ -1,16 +1,260 @@
 <script setup>
 import product from './components/product.vue';
-import {
-    sandwichesProducts,
-    supplementsProducts,
-    desertsProducts,
-    alcoholicProducts,
-    nonAlcoholicProducts,
-    wineProducts,
-    beerProducts,
-    hotDrinkProducts,
-    waterProducts,
-} from '@/utils/products';
+
+const sandwichesProducts = [
+    {
+        name: 'Мортези',
+        image: '',
+        price: 290,
+        description: 'мортадела, страчатела, песто, фокача сендвич, маслиново масло'
+    },
+    {
+        name: 'Пршууто',
+        image: '',
+        price: 310,
+        description: 'свинска пршута, рукола, песто, пармезан, фокача сендвич, балсамико крема, маслиново масло'
+    },
+    {
+        name: 'Пршууто биф',
+        image: '',
+        price: 330,
+        description: 'говедска пршута, рукола, песто, пармезан фокача сендвич, балсамико крема, маслиново масло'
+    },
+    {
+        name: 'Тартуфо',
+        image: '',
+        price: 330,
+        description: 'шунка тартуф, намаз од тартуф, страчатела, рукола, фокача сендвич, маслиново масло'
+    },
+    {
+        name: 'Капрезе',
+        image: '',
+        price: 260,
+        description: 'моцарела, песто, босилек, домат, фокача сендвич, балсамико крема, маслиново масло'
+    },
+    {
+        name: 'Веган',
+        image: '',
+        price: 230,
+        description: 'крем од маслинки, печен зеленчук, тиквички, модал домат, пиперка, сув домат, фокача сендвич, маслиново масло'
+    },
+    {
+        name: 'Веге',
+        image: '',
+        price: 250,
+        description: 'крем од маслинки, печен зеленчук, тиквички, модал домат, пиперка, сув домат, страчатела, фокача сендвич, маслиново масло'
+    },
+];
+
+const supplementsProducts = [
+    {
+        name: 'Страчатела',
+        image: '',
+        price: 40
+    },
+    {
+        name: 'Песто',
+        image: '',
+        price: 30
+    },
+    {
+        name: 'Крем од маслинка',
+        image: '',
+        price: 20
+    },
+];
+
+const desertsProducts = [
+    {
+        name: 'Тирамису',
+        image: '',
+        price: 140,
+        description: 'пишкоти, маскарпоне, слатка павлака, еспресо, какао'
+    },
+];
+
+const alcoholicProducts = [
+    {
+        name: 'Водка смирноф 0.4',
+        image: '',
+        price: 160
+    },
+    {
+        name: 'Водка абсолут 0.4',
+        image: '',
+        price: 170
+    },
+    {
+        name: 'Џин гордонс 0.4',
+        image: '',
+        price: 160
+    },
+    {
+        name: 'Џин бомбај 0.4',
+        image: '',
+        price: 180
+    },
+    {
+        name: 'Виски џејмисон 0.4',
+        image: '',
+        price: 200
+    },
+    {
+        name: 'Виски џек даниелс 0.4',
+        image: '',
+        price: 250
+    },
+    {
+        name: 'Јегермајстер 0.5',
+        image: '',
+        price: 180
+    },
+    {
+        name: 'Кампари 0.6',
+        image: '',
+        price: 180
+    },
+    {
+        name: 'Аперол 0.6',
+        image: '',
+        price: 180
+    },
+];
+
+const nonAlcoholicProducts = [
+    {
+        name: 'Кока кола лим. 0.33',
+        image: '',
+        price: 70
+    },
+    {
+        name: 'Кока кола зеро лим. 0.33',
+        image: '/assets/images/coca-cola-zero-limenka.png',
+        price: 70
+    },
+    {
+        name: 'Кока кола 0.25',
+        image: '',
+        price: 90
+    },
+    {
+        name: 'Кока кола зеро 0.25',
+        image: '',
+        price: 90
+    },
+    {
+        name: 'Фанта 0.25',
+        image: '',
+        price: 90
+    },
+    {
+        name: 'Спрајт 0.25',
+        image: 'assets/images/sprite.png',
+        price: 90
+    },
+    {
+        name: 'Швепс Битер Лемон 0.25',
+        image: 'assets/images/sveps.png',
+        price: 90
+    },
+    {
+        name: 'Швепс танџерина 0.25',
+        image: 'assets/images/sveps-tangerina.png',
+        price: 90
+    },
+    {
+        name: 'Швепс тоник 0.25',
+        image: 'assets/images/sveps.png',
+        price: 90
+    },
+];
+
+const wineProducts = [
+    {
+        name: 'СС бленд бело класик 150мл',
+        image: 'assets/images/casa-belo.png',
+        price: 160
+    },
+    {
+        name: 'СС бленд црвено класик 150мл',
+        image: 'assets/images/casa-crveno.png',
+        price: 160
+    },
+    {
+        name: 'Куве александрија розе 187 мл',
+        image: 'assets/images/alexandria-cuve-roze.png',
+        price: 220
+    },
+    {
+        name: 'Луда мара куве - бело',
+        image: 'assets/images/luda-mara-belo.png',
+        price: 1100
+    },
+    {
+        name: 'Луда мара куве - црвено',
+        image: 'assets/images/luda-mara-crveno.png',
+        price: 1100
+    }
+];
+
+const beerProducts = [
+    {
+        name: 'Скопско',
+        image: 'assets/images/skopsko.png',
+        price: 90
+    },
+    {
+        name: 'Скопско смут',
+        image: 'assets/images/skopsko-smooth.png',
+        price: 90
+    },
+    {
+        name: 'Хајникен',
+        image: 'assets/images/heineken.png',
+        price: 110
+    }
+];
+
+const hotDrinkProducts = [
+    {
+        name: 'Еспресо',
+        image: '',
+        price: 70
+    },
+    {
+        name: 'Мало макијато',
+        image: '',
+        price: 70
+    },
+    {
+        name: 'Големо макијато',
+        image: '',
+        price: 80
+    },
+    {
+        name: 'Капучино',
+        image: '',
+        price: 80
+    },
+    {
+        name: 'Чај',
+        image: '',
+        price: 80
+    },
+];
+
+const waterProducts = [
+    {
+        name: 'Роса негазирана 0.33',
+        image: 'assets/images/rosa.png',
+        price: 70
+    },
+    {
+        name: 'Роса газирана 0.33',
+        image: 'assets/images/rosa-gazirana.png',
+        price: 70
+    }
+];
 
 </script>
 
